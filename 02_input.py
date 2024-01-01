69f8ae3f2dee3b964faae6c370e10ef53f285b1c
nuthapon <nuthaponm79@gmail.com>
2024-01-01 23:21:02 +0700

    feat: new lesson

---
+++ 02_input.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    # input is receive value from terminal or command prompt for window
+    name = input("Enter value")
+    class_name = input("Enter class name")
+    score = input("Enter score")
+    grade = input("Enter grade")
+    is_male = input("Are you male (0 = Male, 1 = Female)")
--- 03_convert_type.py
+++ 03_convert_type.py
@@ -5,23 +5,12 @@ if __name__ == '__main__':
     
     convertor = int, float, bool, list, set, tuple
     """
-    x = int(1)
-    x = float(1)
-    x = str(2)
-    
-    x = bool(0)
-    x = bool(1)
-    
+    name = input("Enter value")
+    class_name = input("Enter class name")
+    score = int(input("Enter score"))
+    grade = float(input("Enter grade"))
     
     # 1.The boolean value is False when the data which is converted is blank or 0
-    x = bool(0)
-    x = bool([])
-    x = bool('')
-    x = bool({})
-    
     # 2. The boolean value is True when the data which is coverted is not blank and not 0
-    x = bool(-1)
-    x = bool(10)
-    x = bool('x')
-    x = bool([[]])
+    is_male = input("Are you male (0 = Male, 1 = Female)")
     
