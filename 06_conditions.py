14ff4ba1a831f5dda1ad4868a0f0b484168403b1
nuthapon <nuthaponm79@gmail.com>
2024-01-01 23:30:49 +0700

    feat: new section

--- 06_conditions.py
+++ 06_conditions.py
@@ -1,12 +1,19 @@
 if __name__ == '__main__':
-    # List: https://www.w3schools.com/python/python_lists.asp
     name = input("Enter value")
     class_name = input("Enter class name")
     score = int(input("Enter score"))
     
     score_percentage = (score/100) * 100
     
-    grade = input("Enter grade")
+    if score >= 80:
+        grade = 'A'
+    elif score >= 60:
+        grade = 'B'
+    elif score >= 50:
+        grade = 'C'
+    else:
+        grade = 'D'
+        
     is_male = input("Are you male (0 = Male, 1 = Female)")
     
     headers = ['Name', 'Student ID', 'Score', 'Grade']
