457944b05e6b8029fc49e88029e8a9b09408b26d
nuthapon <nuthaponm79@gmail.com>
2024-01-01 23:09:10 +0700

    feat: variable

---
+++ 01_variable_data_type.py
@@ -0,0 +1,35 @@
+if __name__ == '__main__':
+    """
+    Variable assignment rule
+    1. First of variable name is not number and special character except underscore (_)
+    2. First of variable name is not number
+    3. Case sensitive (Upper variable name is not same lower variable name: Ex is_male, IS_MALE)
+    
+    Variable assignment clean code
+    1. Python variable is snake case 
+    Ex. name, class_name, address
+    
+    2. Python is high level language. It is not neccessary for assign data type
+    
+    3. For value of variable name is boolean, assign like question varaible.
+    Ex is_male, has_data
+    
+    4. Variable assignment can be readable.
+    Remark: Don't assign short variable except i (index)
+    Ex: s = 20, x = 30
+    """
+    
+    # Remark assignment with data type
+    name: str = 'John'
+    class_name: str = 'S001'
+    score: int = 20
+    grade: str = 'A'
+    is_male: bool = True
+    
+    
+    name = 'John'
+    class_name = 'S001'
+    score = 20
+    grade = 'A'
+    is_male = True
+    
